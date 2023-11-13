788478741fac10a3fc90290fcca9dc4998a9f7c6
Airme1 <emmanuelochoja@yahoo.com>
2023-11-13 03:39:30 +0100

    clear changes

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,9 +1,1 @@
-#!/usr/bin/python3
-"""Creating a file sotrage module to use at
-the instant the application begins to run
-"""
 
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
